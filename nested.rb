6f711613fb0c14b17561e399f698d9424a8ee8f0
Luke Thinnes <lukethinnes@yahoo.com>
2020-11-19 05:18:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,10 +59,6 @@ end
 def adding_matz
 	programmer_hash =
  		{
-			:yukihiro_matsumoto => {
-				:known_for => "Ruby",
-				:languages => ["LISP, C"]
-			},
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
