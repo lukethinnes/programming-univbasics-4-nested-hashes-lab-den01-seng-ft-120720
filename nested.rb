df907c7134013523bf9e9c8688a79b55a274e2d3
Luke Thinnes <lukethinnes@yahoo.com>
2020-11-19 05:14:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -71,10 +71,10 @@ def adding_matz
         :known_for => "Unix",
         :languages => ["C"]
       }
-		}
+		},
 		programmer_hash.[:yukihiro_matsumoto] = {
 			:known_for => "Ruby",
-			:languages => ["LISP", "C"]
+			:languages => ["LISP", "C",],
 		}
 		programmer_hash
 end
